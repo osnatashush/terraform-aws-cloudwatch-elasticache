07ead113477270baee8fdf056c45b05e823f6da5
osnatashush <osnat@delivops.com>
2024-12-02 12:25:10 +0200

    fixing

--- elasticache-alarms.tf
+++ elasticache-alarms.tf
@@ -23,7 +23,7 @@ resource "aws_cloudwatch_metric_alarm" "high_cpu" {
 
 }
 resource "aws_cloudwatch_metric_alarm" "high_connection" {
-  for_each                  = toset(local.cache_nodes_ids) * var.high_connection_enabled ? 1 : 0
+  for_each                  = var.high_connection_enabled ? toset(local.cache_nodes_ids) : {}
   alarm_name                = "${var.cache_cluster_id}-node-${each.value}-status-check-failed"
   comparison_operator       = "LessThanThreshold"
   evaluation_periods        = 10
